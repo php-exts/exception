933b46bd45fa7b34452f724a77c49232cf392fe2
imxieke <oss@live.hk>
2025-10-18 09:30:51 +0800

    Add ExtensionNotFoundException

---
+++ src/ExtensionNotFound.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Zeus\Exception;
+
+/**
+ * Extension Not Found Exception
+ *
+ * @author imxieke <oss@live.hk>
+ * @copyright (c) 2025 CloudFlying
+ * @date 2025/06/03 21:15:29
+ */
+class ExtensionNotFound extends Exception {}
