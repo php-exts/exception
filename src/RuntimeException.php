fa87aa6cb884de54af33224550b053158299a36f
imxieke <oss@live.hk>
2025-10-30 14:53:30 +0800

    add RuntimeException

---
+++ src/RuntimeException.php
@@ -0,0 +1,13 @@
+<?php
+declare (strict_types = 1);
+
+namespace Zeus\Exception;
+
+/**
+ * Runtime Exception
+ *
+ * @author imxieke <oss@live.hk>
+ * @copyright (c) 2025 CloudFlying
+ * @date 2025/10/30 14:51:22
+ */
+class RuntimeException extends \RuntimeException {}
